af8eb981a79f075a937061c8705b93f55fba35eb
Neil-Davison <neil.davison@grdc.com.au>
2025-07-31 10:31:15 +1000

    Change input data source

--- tests/dynamicCategories.spec.ts
+++ tests/dynamicCategories.spec.ts
@@ -7,7 +7,7 @@ import { ProductPage } from '../pages/demoWebShop_Product';
 
 // Load YAML data
 const testData = yaml.load(
-  fs.readFileSync('C:\\Users\\NeilDavison\\OneDrive - Elysium Digital\\Documents\\testData1.yaml', 'utf8')
+    fs.readFileSync('C:\\Users\\NeilDavison\\PlaywrightTest\\test-data\\testData1.yaml', 'utf8')
 ) as any;
 
 for (const user of testData.users) {
